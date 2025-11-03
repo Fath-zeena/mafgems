060e5424c836ff1305a13e7530c0ac8830686092
[dyad] <webprojects@email.com>
2025-11-03 22:42:29 +0530

    [dyad] Refined spot light for softer, more spread-out illumination - wrote 1 file(s)

--- src/components/ring-viewer.tsx
+++ src/components/ring-viewer.tsx
@@ -130,8 +130,8 @@ export function RingViewer() {
     >
       <Canvas camera={{ position: [3, 2, 3], fov: 45 }}>
         <ambientLight intensity={Math.PI} />
-        {/* Adjusted spotLight for a more realistic, less washed-out top view */}
-        <spotLight position={[0, 5, 0]} angle={0.5} penumbra={0.8} decay={0} intensity={Math.PI / 2} />
+        {/* Adjusted spotLight for a softer, more spread-out light */}
+        <spotLight position={[0, 6, 0]} angle={0.7} penumbra={1} decay={0} intensity={Math.PI / 3} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         
         <Ring />
