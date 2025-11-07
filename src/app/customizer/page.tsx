27d87be240090d3f28d12b5019b1da92dacc825c
[dyad] <webprojects@email.com>
2025-11-07 23:33:28 +0530

    [dyad] Restored the ring customizer to its fully working state by removing the broken necklace feature. - wrote 2 file(s), deleted 2 file(s)

--- src/app/customizer/page.tsx
+++ src/app/customizer/page.tsx
@@ -4,18 +4,9 @@ import { GemGallery } from "@/components/gem-gallery";
 import dynamic from "next/dynamic";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CustomizerProvider } from "@/context/customizer-context";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const DynamicRingViewer = dynamic(
   () => import("@/components/ring-viewer").then((mod) => mod.RingViewer),
-  {
-    ssr: false,
-    loading: () => <Skeleton className="w-full h-full" />,
-  }
-);
-
-const DynamicNecklaceViewer = dynamic(
-  () => import("@/components/necklace-viewer").then((mod) => mod.NecklaceViewer),
   {
     ssr: false,
     loading: () => <Skeleton className="w-full h-full" />,
@@ -30,22 +21,8 @@ export default function CustomizerPage() {
           <h1 className="text-2xl font-bold mb-4">Jewelry Customizer</h1>
           <GemGallery />
         </aside>
-        <main className="flex-1 h-full flex flex-col">
-          <Tabs defaultValue="ring" className="w-full flex flex-col flex-grow">
-            <TabsList className="mx-auto mt-2 flex-shrink-0">
-              <TabsTrigger value="ring">Ring</TabsTrigger>
-              <TabsTrigger value="necklace">Necklace</TabsTrigger>
-            </TabsList>
-            {/* This container is the key. It's relative and will grow to fill the remaining space. */}
-            <div className="relative flex-grow w-full">
-              <TabsContent value="ring" className="absolute inset-0 w-full h-full">
-                <DynamicRingViewer />
-              </TabsContent>
-              <TabsContent value="necklace" className="absolute inset-0 w-full h-full">
-                <DynamicNecklaceViewer />
-              </TabsContent>
-            </div>
-          </Tabs>
+        <main className="flex-1 h-full">
+          <DynamicRingViewer />
         </main>
       </div>
     </CustomizerProvider>
