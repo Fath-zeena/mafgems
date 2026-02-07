d33ed626bb8f5573bd938999eb22089ba3e8ae59
[dyad] <Fath-zeena@hotmail.com>
2026-02-07 07:44:46 +0530

    [dyad] Removing problematic webpack config to fix build error - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -13,17 +13,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    if (process.env.NODE_ENV === "development") {
-      config.module.rules.push({
-        test: /\.(jsx|tsx)$/,
-        exclude: [/node_modules/, /ring-viewer\.tsx$/, /ring-viewer-client\.tsx$/],
-        enforce: "pre",
-        use: "@dyad-sh/nextjs-webpack-component-tagger",
-      });
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
